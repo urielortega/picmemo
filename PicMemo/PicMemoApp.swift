3474e2f440bb4eb4d9f49c3456d288e7dcbe987c
Uriel Ortega <urielortega2509@gmail.com>
2023-06-28 12:25:50 -0600

    Initial Commit

---
+++ PicMemo/PicMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicMemoApp.swift
+//  PicMemo
+//
+//  Created by Uriel Ortega on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PicMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
